e6475fd7163a6c1de94add7175e4811f52f31919
Grzegorz Klimek <kfiku.com@gmail.com>
2018-05-16 13:47:28 +0200

    fix lint

--- src/index.test.js
+++ src/index.test.js
@@ -3,14 +3,13 @@
 /* eslint-disable fp/no-unused-expression */
 /* eslint-disable fp/no-nil */
 
-const { join } = require('path')
 const { developersCommitsInDates } = require('./index')
 
 jest.mock('./helpers/exec')
 
 describe('developersCommitsInDates', () => {
   it('should work fine', async () => {
-    const result = await developersCommitsInDates([''], new Date(), new Date())
+    const result = await developersCommitsInDates([''], new Date(), new Date(), () => {})
     expect(result).toMatchSnapshot()
   })
 })
